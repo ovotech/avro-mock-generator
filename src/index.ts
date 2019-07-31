fdd3ecfbf04c75f8043d733362103113733ca375
Nicolas Lagoutte <nicolas.lagoutte@ovoenergy.com>
2019-07-31 12:56:06 +0100

    Support all avro primitive types

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+import uuid from 'uuid/v4';
+
+const typeGenerators = {
+  int: () => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+  // This  doesn't technically produce a long but JS doesn't support those, so it's likely the user will have a logicalType on top of it
+  long: () => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+  double: () => Math.random(),
+  float: () => Math.random(),
+  null: () => null,
+  boolean: () => Boolean(Math.round(Math.random())),
+  string: () => uuid(),
+  bytes: () => Buffer.from(uuid(), 'ascii'),
+};
+
 function generateDataForType(type) {
-  switch (type) {
-    case 'int':
-      return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-    default:
-      if (typeof type === 'object') {
-        if (type.type === 'record') {
-          return generateRecord(type);
-        }
-      }
+  if (typeGenerators[type]) {
+    return typeGenerators[type]();
+  }
+
+  if (typeof type === 'object') {
+    if (type.type === 'record') {
+      return generateRecord(type);
+    }
   }
   throw new Error(`Unknown type ${type}`);
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,12 +1,53 @@
 import generateData from '../src/index';
 
 describe('Avro mock data generator', () => {
+  it('supports all avro primitive types', () => {
+    const result = generateData({
+      type: 'record',
+      fields: [
+        { name: 'int', type: 'int' },
+        { name: 'long', type: 'long' },
+        { name: 'double', type: 'double' },
+        { name: 'float', type: 'float' },
+        { name: 'boolean', type: 'boolean' },
+        { name: 'null', type: 'null' },
+        { name: 'string', type: 'string' },
+        { name: 'bytes', type: 'bytes' },
+      ],
+    });
+
+    expect(result).toMatchObject({ int: expect.any(Number) });
+    expect(result).toMatchObject({ float: expect.any(Number) });
+    expect(result).toMatchObject({ double: expect.any(Number) });
+    expect(result).toMatchObject({ long: expect.any(Number) });
+    expect(result).toMatchObject({ boolean: expect.any(Boolean) });
+    expect(result).toMatchObject({ string: expect.any(String) });
+    expect(result).toMatchObject({ null: null });
+    expect(result).toMatchObject({ bytes: expect.any(Buffer) });
+  });
+
   it('can parse a basic schema', () => {
     const result = generateData({
       type: 'record',
       fields: [{ name: 'nbChickens', type: 'int' }],
     });
     expect(result).toEqual({ nbChickens: expect.any(Number) });
+  });
+
+  it('can parse a record field', () => {
+    const result = generateData({
+      type: 'record',
+      fields: [
+        {
+          name: 'farm',
+          type: {
+            type: 'record',
+            fields: [{ name: 'nbChickens', type: 'int' }],
+          },
+        },
+      ],
+    });
+    expect(result).toEqual({ farm: { nbChickens: expect.any(Number) } });
   });
 
   it('throws when encountering an unknown type', () => {
