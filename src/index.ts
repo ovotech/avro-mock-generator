43607e22bcb55c923ed08232c94c673ed9f1aff2
Nicolas Lagoutte <nicolas.lagoutte@ovoenergy.com>
2019-07-31 12:04:37 +0100

    Start to write the generator

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export default () => {
-  return 'something'
+function generateDataForType(type) {
+  switch (type) {
+    case 'int':
+      return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    default:
+      if (typeof type === 'object') {
+        if (type.type === 'record') {
+          return generateRecord(type);
+        }
+      }
+  }
+  throw new Error(`Unknown type ${type}`);
 }
+
+function generateRecord({ fields }) {
+  return fields.reduce((record, { name, type }) => {
+    record[name] = generateDataForType(type);
+    return record;
+  }, {});
+}
+
+// TODO support logical types
+export default (schema: any) => {
+  return generateRecord(schema);
+};
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,7 +1,20 @@
 import generateData from '../src/index';
 
 describe('Avro mock data generator', () => {
-  it('exists', () => {
-    expect(generateData).not.toBeUndefined();
+  it('can parse a basic schema', () => {
+    const result = generateData({
+      type: 'record',
+      fields: [{ name: 'nbChickens', type: 'int' }],
+    });
+    expect(result).toEqual({ nbChickens: expect.any(Number) });
+  });
+
+  it('throws when encountering an unknown type', () => {
+    const schema = {
+      type: 'record',
+      fields: [{ name: 'nbChickens', type: '3rd Kind' }],
+    };
+
+    expect(() => generateData(schema)).toThrow('Unknown type');
   });
 });
